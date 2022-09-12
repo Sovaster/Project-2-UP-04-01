0fa92a3a5108ede3e207685a60ee28a4e80be9da
Sovaster <sovaster.n@mail.ru>
2022-09-12 13:17:09 +0300

    5

--- src/main/java/com/example/Project2/config/WebSecurityConfig.java
+++ src/main/java/com/example/Project2/config/WebSecurityConfig.java
@@ -32,7 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/registration").permitAll()
+                .antMatchers("/registration").permitAll()
                 //.antMatchers("/blog").permitAll()
                 .anyRequest().authenticated()
                 .and()
